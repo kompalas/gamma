75c1b672f874de767adfd02ae7099e00fd296be2
Konstantinos Balaskas <kompalas@physics.auth.gr>
2023-05-13 10:20:26 +0000

    saving results and adding cifar models

--- eval.sh
+++ eval.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-resdir=${1:-"outdir/GAMMA_resnet50_SL-2-2_F1-area_GEN-50_POP-100_Area-10000000.0_MaxPEs-10000_CostModelCstr-maestro_cstr/"}
+resdir=${1?}
 
 python3 evaluation/evaluate.py \
 	--resdir $resdir
--- run_gamma.sh
+++ run_gamma.sh
@@ -1,20 +1,23 @@
+#!/bin/bash
+model="${1?}"
+
 mkdir -p outdir/
 cd ./src/GAMMA
 python main.py \
-	--model resnet50 \
-	--fitness1 area \
-	--fitness2 energy \
+	--model $model \
+	--fitness1 energy \
+	--fitness2 latency \
 	--num_pop 100 \
-	--epochs 50 \
+	--epochs 200 \
 	--num_pe -1 \
 	--l1_size -1 \
 	--l2_size -1 \
-	--area_budget 10000000 \
-	--pe_limit 10000 \
-	--NocBW 81920000 \
-	--offchipBW 1073741824 \
+	--pe_limit 100000 \
+	--area_budget 1000000 \
 	--log_level 2 \
 	2>&1 | tee ../../outdir/last_run.log
+	#--NocBW 81920000 \
+	#--offchipBW 1073741824 \
 cd ../../
 
 
