7721b75bc3af04b32a3d4131f946a19d104d9d8a
root <Konstantinos Balaskas>
2023-05-17 05:38:11 +0000

    forcing area constraint with second objective

--- run_gamma.sh
+++ run_gamma.sh
@@ -2,6 +2,7 @@
 # NOTE: Area budget is in mm^2. So argument is multiplied with 1e+6
 
 model="${1?}"
+area_budget="${2?}"
 
 mkdir -p outdir/
 cd ./src/GAMMA
@@ -14,12 +15,12 @@ python main.py \
 	--num_pe -1 \
 	--l1_size -1 \
 	--l2_size -1 \
-	--pe_limit 100000 \
-	--area_budget 1 \
+	--NocBW 81920000 \
+	--offchipBW 1073741824 \
+	--pe_limit 1000000 \
+	--area_budget $area_budget \
 	--log_level 2 \
 	2>&1 | tee ../../outdir/${model}.log
-	#--NocBW 81920000 \
-	#--offchipBW 1073741824 \
 cd ../../
 
 
